0b8e1d543911d72757c63b586d38054f03f7c02d
duyanh-coder <cdanha18037@cusc.ctu.edu.vn>
2022-04-14 00:16:33 +0700

    Init Using Hook project

---
+++ usinghook/src/usingState.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useState } from 'react';
+
+// const orders = [100, 200, 300]
+
+export default function () {
+
+//     // use callback init State
+//     const [counter, setCounter] = useState(() => {
+//         const total = orders.reduce((total, current) => total + current)
+//         return total
+//     })
+
+//     // use callback setState
+//     const handleIncrease = () => {
+//         setCounter(prevCounter => prevCounter +1)
+//     }
+
+//   return (
+//     <div style={{ padding: 20 }} >
+//         <h3>Counter using useState()</h3>
+
+//         <h1>{counter}</h1>
+//         <button onClick={handleIncrease} >Increase</button>
+
+//     </div>
+//   )
+
+//==============================================================
+
+    // change or update State
+    const [info, setInfo] = useState({
+        name: 'Chau Duy Anh',
+        age: 42,
+        address: 'Ninh Kieu - TPCT'
+    })
+
+    const handleChange = () => {
+        setInfo({
+            ...info,        //Spread operator
+            job: 'Front-End Developer'
+        })
+    }
+    return (
+        <div style={{ padding: 20 }} >
+            <h3>Change State</h3>
+
+            <h1>{JSON.stringify(info)}</h1>
+            <button onClick={handleChange} >Update</button>
+
+        </div>
+    )
+
+}
